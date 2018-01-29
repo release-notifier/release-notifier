2aa0e9500f33920fe9ef2607b0aef52b10a1c7f8
Zeke Sikelianos <zeke@sikelianos.com>
2018-01-29 12:29:51 -0800

    add more robust detection of previous version

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 const {chain} = require('lodash')
+const semver = require('semver')
 
 module.exports = (robot) => {
-  robot.log('hello from the robot')
-
   robot.on('release.published', handleRelease.bind(null, robot))
-  robot.on('*', async context => {
-    robot.log('hello! an event..')
-    robot.log(Object.keys(context))
-    // robot.log(context.payload)
+  robot.on('*', context => {
+    // robot.log(context)
   })
 }
 
 async function handleRelease (robot, context) {
-  // robot.log(context.payload)
-  robot.log(context.payload.release.body)
-  
   const owner = context.payload.repository.owner.login
   const repo = context.payload.repository.name
-  const {data: [latestRelease, previousRelease]} = await context.github.repos.getReleases({
+  const currentVersion = tagNameToVersionNumber(context.payload.release.tag_name)
+  const {data: releaseData} = await context.github.repos.getReleases({
     owner,
     repo,
-    per_page: 2
+    per_page: 50
   })
 
-  // TODO: will break on repo with single release
-  robot.log(latestRelease.tag_name, previousRelease.tag_name)
+  recentReleases = releaseData
+    .map(release => {
+      release.version = tagNameToVersionNumber(release.tag_name)
+      return release
+    })
+    .filter(release => semver.valid(release.version))
+    .sort((a, b) => semver.compare(b.version, a.version))
+
+  const currentRelease = recentReleases
+    .find(release => release.tag_name === context.payload.release.tag_name)
   
+  const previousReleases = recentReleases
+    .filter(release => !release.prerelease)
+    .filter(release => semver.lt(release.version, currentRelease.version))
+  
+  if (!previousReleases.length) {
+    robot.log('aborting; no previous releases found with a lower semnantic version')
+    return
+  }
+
+  const previousRelease = previousReleases[0]
+
   const {data: commits} = await context.github.repos.getCommits({
     owner,
     repo,
-    sha: latestRelease.tag_name,
+    sha: currentRelease.tag_name,
     since: previousRelease.created_at
   })
   
-  robot.log(commits.length)
+  robot.log(`commits between ${previousRelease.version} and ${currentRelease.version}: ${commits.length}`)
+
+  // Create a nice title without redundant version info
+  currentRelease.title = currentRelease.name.includes(currentRelease.version)
+    ? currentRelease.name
+    : `${currentRelease.tag_name}: ${currentRelease.name}`
     
   const pullRequests = chain(commits)
     .map(async commit => {
       const {data: {items: [pullRequest]}} = await context.github.search.issues({q: commit.sha})
-
       return pullRequest
     })
     .compact()
@@ -48,8 +66,12 @@ async function handleRelease (robot, context) {
         owner,
         repo,
         number: (await pullRequest).number,
-        body: `This PR landed in [${latestRelease.name}](${latestRelease.html_url})`
+        body: `This PR landed in [${currentRelease.title}](${currentRelease.html_url}) :tada:`
       })
     })
     .value()
+}
+
+function tagNameToVersionNumber(tag) {
+  return tag.replace(/^v/i, '')
 }
